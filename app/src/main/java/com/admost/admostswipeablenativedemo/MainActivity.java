ec2fc31340136b951ad5e7f5c6ed95d4205e7870
hanilozmen <hanilozmen@gmail.com>
2020-06-04 12:19:30 +0300

    AdMostStory;y View Provider implemented

--- app/src/main/java/com/admost/admostswipeablenativedemo/MainActivity.java
+++ app/src/main/java/com/admost/admostswipeablenativedemo/MainActivity.java
@@ -3,24 +3,21 @@ package com.admost.admostswipeablenativedemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-
-import androidx.annotation.NonNull;
-
+import com.amr.plugin.storyly.AdMostStorylyViewProvider;
 import com.appsamurai.storyly.StorylyInit;
 import com.appsamurai.storyly.StorylyView;
-import com.appsamurai.storyly.external.StorylyExternalViewListener;
-import com.appsamurai.storyly.external.StorylyExternalViewProvider;
 
 import admost.sdk.base.AdMost;
 import admost.sdk.base.AdMostConfiguration;
 import admost.sdk.listener.AdMostInitListener;
 
 public class MainActivity extends Activity {
-    private StorylyAdView storylyAdView;
 
     public static final String TAG = "ADMOST_SAMPLE_APP";
 
     public static final String ADMOST_APP_ID = "6cc8e89a-b52a-4e9a-bb8c-579f7ec538fe";
+    public static final String NATIVE_ZONE_ID = "9e5d4b82-aa93-43a1-9fd5-3c71691fbcc7";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,43 +40,7 @@ public class MainActivity extends Activity {
         StorylyView storylyView = findViewById(R.id.storyly_view);
         storylyView.setStorylyInit(new StorylyInit("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjU1NiwiYXBwX2lkIjozMTksImluc19pZCI6MzE5fQ.AMVZQxwyXO0mSWFhuxOsNAv8kOXZnSvniyKHV2-izFk"));
         // StorylyExternalProvider listener will handle upcoming request flow for ads
-        storylyView.setStorylyExternalViewProvider(new StorylyExternalViewProvider() {
-            @Override
-            public void onRequest(@NonNull StorylyExternalViewListener storylyExternalViewListener) {
-                createStorylyAdView(storylyExternalViewListener);
-            }
-        });
-
-
+        storylyView.setStorylyExternalViewProvider(new AdMostStorylyViewProvider(this,NATIVE_ZONE_ID ));
     }
 
-    private void createStorylyAdView(@NonNull StorylyExternalViewListener storylyExternalViewListener) {
-        if(storylyAdView == null) {
-            storylyAdView = new StorylyAdView(this, storylyExternalViewListener);
-        }else {
-            storylyAdView.storylyExternalViewListener = storylyExternalViewListener;
-            storylyAdView.refreshAd();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (storylyAdView != null)
-            storylyAdView.resume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (storylyAdView != null)
-            storylyAdView.pause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (storylyAdView != null)
-            storylyAdView.destroy();
-    }
 }
